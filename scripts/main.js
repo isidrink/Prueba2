58f484e1f1fec507901036c24cea44f72c77cf69
isidrink <isidro.escoda@gmail.com>
2013-01-21 13:58:46 +0100

    Prueba 2 commit1

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+
+// JavaScript Document
+ 
+// Wait for PhoneGap to load
+document.addEventListener("deviceready", onDeviceReady, false);
+ 
+// PhoneGap is ready
+function onDeviceReady() {
+}
+//=======================Ajax View=======================//
+function showAjaxView() {
+    getAjax();
+}
+ 
+function getAjax(){
+    $.ajax({
+            type:"GET",
+            url: "https://api.stackexchange.com/2.1/info?site=stackoverflow"
+        }).done(function( data ) {
+               updateStackStatListView(data);
+            });
+}
+ 
+function updateStackStatListView( data ) {
+    alert(JSON.stringify(data));
+    $("#stackStats").kendoMobileListView({
+        dataSource: kendo.data.DataSource.create({data: data.items}),
+        template: $("#stackStatsTemplate").html()
+    });
+}
